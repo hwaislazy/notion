8fdd524ac7f3b0e93c6a02ab2cb8b53b1dbb6445
hwaislazy <123188864+hwaislazy@users.noreply.github.com>
2023-02-25 03:34:41 +0900

    Add files via upload

---
+++ interactive-record-player-jquery/README.md
@@ -0,0 +1,5 @@
+# Interactive Record Player jQuery
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/hwaislazy/pen/WNgGoNy](https://codepen.io/hwaislazy/pen/WNgGoNy).
+
+A vintage style Record Player that can play/ pause your music. Control volume with range slider. 
---
+++ interactive-record-player-jquery/dist/script.js
@@ -0,0 +1,25 @@
+var state=0;
+		$(window).load(function(){
+             $(".loader").hide();
+             $(".player").show();
+        });
+		$(document).ready(function(){
+			$(".power").click(function(){
+  				$(".record").toggleClass('on');
+  				$(".stick").toggleClass('play');
+  				if(state==0){
+  					setTimeout(function(){
+  					 $(".mysong")[0].play();
+  					},1000);
+  					state=1;
+  				}
+  				else{
+  					$(".mysong")[0].pause();
+  					state=0;
+  				}
+  				$(".slider").change(function(){
+  					$(".mysong")[0].volume=this.value;
+  				});
+  			});
+  			
+		});
